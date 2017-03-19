5e72d7f0a7c3a785285e72f487eee2abb732fec4
devopsec <tyler.moore58@gmail.com>
2017-03-18 23:25:05 -0400

    clean up build and binary files not needed

--- libraries/cmake-build-debug/CMakeFiles/biitest.dir/cmake_clean.cmake
+++
@@ -1,5 +0,0 @@
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/biitest.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
--- libraries/cmake-build-debug/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /pwdmanlib/libraries
-# Build directory: /pwdmanlib/libraries/cmake-build-debug
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
